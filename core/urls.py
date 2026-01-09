7e21369161a72694f9fa14542d830170eb11e05d
clara <claraavelino19@gmail.com>
2026-01-08 23:24:23 -0300

    feat: adicionada funcionalidade de leitura de PDF e capas clicáveis

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bookshelf.urls')), # Esta linha conecta as rotas do seu app
+    path('', include('bookshelf.urls')),
 ]
 
-# Adicione isto para que o CSS e as Imagens apareçam no navegador:
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
